2591c4c3b838136af66202a8bb25686349a31aaf
Abhiraksh <abhirakshkesarwani@gmail.com>
2020-11-08 12:06:59 +0800

    Files Modified

--- Chain.js
+++ Chain.js
@@ -1,20 +1,28 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
+        this.pointB = pointB;
         World.add(world, this.chain);
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+     }
+     }
+
+    release(){
+        this.chain.bodyA = null;
     }
+    
     
 }
